da4eead5097cf4ba85d3d9e406c093eaa102cdde
jlosaw <jeremy.losaw@enventyspartners.com>
2020-02-11 21:19:50 -0500

    Create sdq_blynk_dht11.ino

---
+++ sdq_blynk_dht11.ino
@@ -0,0 +1,54 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_DHT.h>
+
+// This #include statement was automatically added by the Particle IDE.
+#include <blynk.h>
+
+
+#define BLYNK_PRINT Serial  // Set serial output for debug prints
+//#define BLYNK_DEBUG       // Uncomment this to see detailed prints
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "2fd02368df2749cbabafb6053834d03f";
+
+//DHT Setup
+
+#define DHTPIN 2     // what pin we're connected to
+
+#define DHTTYPE DHT11		// DHT 11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup()
+{
+  
+    Blynk.begin(auth);
+    delay (1000);
+    dht.begin();
+}
+
+
+
+
+void loop()
+{
+    
+    Blynk.run();
+    
+    	float h = dht.getHumidity();
+    	float f = dht.getTempFarenheit();
+    	
+    	// Check if any reads failed and exit early (to try again).
+	if (isnan(h)  || isnan(f)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+
+
+Blynk.virtualWrite(V3, h);
+Blynk.virtualWrite(V4, f);
+
+    	
+}
